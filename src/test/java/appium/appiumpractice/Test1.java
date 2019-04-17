f53be9fa61456711e890e8cafacfb8d899729964
DanSchon <danyschonfeld@gmail.com>
2019-04-17 12:26:10 -0700

    added code to navigate back to home page

--- src/test/java/appium/appiumpractice/Test1.java
+++ src/test/java/appium/appiumpractice/Test1.java
@@ -3,17 +3,28 @@ package appium.appiumpractice;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 
-import org.testng.annotations.Test;
 
 public class Test1 extends TestBase {
 
 	@Test
-	public void SubmitText() throws MalformedURLException, InterruptedException {
+	public void TapOnTwoTabsThenSubmitTextAndNavigateBackToHomePage() throws MalformedURLException, InterruptedException {
 		setUp();
+		
+		System.out.println("tap on 'Alert Views'");
 		driver.findElementById("Alert Views").click(); // use 'findElementById' for 'accessibilityId' locators
+		
+		System.out.println("tap on 'Text Entry'");
 		driver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"Text Entry\"]").click();
+		
+		System.out.println("type 'hello' on the text field that appears");
 		driver.findElementByClassName("XCUIElementTypeTextField").sendKeys("hello"); // use 'findElementByClassName' for 'type' locators
+		
+		System.out.println("tap on 'OK' button to submit form");
 		driver.findElementByName("OK").click();
+		
+		System.out.println("go back to home page");
+		driver.navigate().back();
+		
 		tearDown();
 	}
 	
