c4d7ac93769432ca719ac2a155b73a813abf067f
DanSchon <danyschonfeld@gmail.com>
2019-05-02 09:00:06 -0700

    created automated test for safari mobile browser

--- src/test/java/appium/appiumpractice/safariBrowserOnIOs/Test1.java
+++ src/test/java/appium/appiumpractice/safariBrowserOnIOs/Test1.java
@@ -2,6 +2,7 @@ package appium.appiumpractice.safariBrowserOnIOs;
 
 import java.net.MalformedURLException;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
@@ -11,6 +12,8 @@ public class Test1 extends TestBaseSafari {
 	public void test() throws InterruptedException, MalformedURLException {
 		super.setUp();
 		driver.get("https://www.aaa.com/stop/");
+		driver.findElement(By.id("zipCode")).sendKeys("90094");
+		driver.findElement(By.id("goButton")).click();
 		Thread.sleep(30000);
 		super.tearDown();
 	}
