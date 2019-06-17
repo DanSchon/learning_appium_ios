dd83083604802e0e2acabaf37d7b0aeda1c9216b
DanSchon <danyschonfeld@gmail.com>
2019-06-16 21:02:03 -0700

    updated ios simulator

--- src/test/java/appium/appiumpractice/nativeIOs/TestBaseNativeIOs.java
+++ src/test/java/appium/appiumpractice/nativeIOs/TestBaseNativeIOs.java
@@ -24,9 +24,9 @@ public class TestBaseNativeIOs {
 		
 		public void setUp() throws MalformedURLException {
 			DesiredCapabilities d = new DesiredCapabilities();
-			d.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
+			d.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Xr");
 			d.setCapability(MobileCapabilityType.PLATFORM_NAME, "IOS");
-			d.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.4");
+			d.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.2");
 			d.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
 			d.setCapability(MobileCapabilityType.APP, "/Users/danielschonfeld/Desktop/UICatalog.app");
 			driver = new IOSDriver<IOSElement>(new URL("http://127.0.0.1:4723/wd/hub"), d);
